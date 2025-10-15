bdfb5ae4c0ab18ff3fc60949dfae87f432ca9a30
lalala_233 <lalala_233@qq.com>
2025-10-15 19:44:58 +0800

    feat: 1243-2

---
+++ 1243-2.c
@@ -0,0 +1,18 @@
+// 乘积符号
+#include <stdint.h>
+#include <stdio.h>
+
+int main(void) {
+    int32_t a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    size_t negative_count = (int)(a < 0) + (int)(b < 0) + (int)(c < 0);
+
+    if(negative_count % 2 == 0)
+    {
+        printf("1\n");
+    } else
+    {
+        printf("-1\n");
+    }
+    return 0;
+}
