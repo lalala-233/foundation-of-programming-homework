e45ba96c90d6218888d1ea94217f88303b4981a0
lalala_233 <lalala_233@qq.com>
2025-11-27 23:11:25 +0800

    feat: 4

--- 1324/4.c
+++ 1324/4.c
@@ -43,49 +43,85 @@ char* clone(const char* s, int32_t len) {
 }
 void process(const char* s, const int32_t len) {
     char* s_clone = clone(s, len);
-    int32_t new_len = len;
-    Command command = End;
+    int32_t str_len = len;
+    Command command = Get;
     while (true) {
         char current = getchar();
         if (command == Get && isspace(current)) {
             free(s_clone);
             s_clone = clone(s, len);
-            int32_t new_len = len;
+            str_len = len;
             continue;
         }
-        switch (parse_command(command)) {
+        command = parse_command(current);
+        switch (command) {
             case Delete: {
-                int32_t length;
-                scanf("%" PRIi32, &length);
+                int32_t delete_length;
+                scanf("%" PRIi32, &delete_length);
                 // first_right(0..=5) = 2
                 // first_right(0..=4) = 2
                 // first_right(0..=3) = 1
                 // first_right(0..=2) = 1
                 // first_right(0..=1) = 0
-                int32_t first_right = new_len / 2;
-                int32_t copy_end = max(first_right - length, 0);
-                char* new = malloc(sizeof(char) * (copy_end + new_len))
+                int32_t first_right = str_len / 2;
 
-                    break;
+                int32_t new_len = str_len - min(first_right, delete_length);
+                char* new = malloc(sizeof(char) * (new_len + 1));
+
+                int32_t copy_end = max(first_right - delete_length, 0);
+                int32_t index = 0;
+                for (; index < copy_end; index++) {
+                    new[index] = s_clone[index];
+                }
+                for (int32_t j = 0; index < new_len; j++) {
+                    new[index] = s_clone[j + first_right];
+                    index++;
+                }
+                new[index] = '\0';
+
+                free(s_clone);
+                s_clone = new;
+                str_len = new_len;
+                break;
             }
             case End: {
+                free(s_clone);
                 return;
             }
             case Get: {
                 int32_t index;
                 scanf("%" PRIi32, &index);
-                printf("%c\n", s_clone[index - 1]);
+                if (index > str_len || index < 1) {
+                    printf("NULL\n");
+                } else {
+                    printf("%c\n", s_clone[index - 1]);
+                }
                 break;
             }
             case Lengthen: {
+                int32_t new_len = str_len * 2;
+                char* new = malloc(sizeof(char) * (new_len + 1));
+                for (int32_t i = 0; i < str_len; i++) {
+                    new[2 * i] = s_clone[i];
+                    new[2 * i + 1] = s_clone[i];
+                }
+                new[new_len] = '\0';
+                free(s_clone);
+                s_clone = new;
+                str_len = new_len;
                 break;
             }
             case Reverse: {
+                for (int32_t i = 0; i < str_len / 2; i++) {
+                    char temp = s_clone[i];
+                    s_clone[i] = s_clone[str_len - i - 1];
+                    s_clone[str_len - i - 1] = temp;
+                }
                 break;
             }
         }
+        // printf("command: %c, current: %s\n", current, s_clone);
     }
-    char character = getchar();
 }
 
 int32_t main(void) {
@@ -97,4 +133,5 @@ int32_t main(void) {
         len--;
     }
     process(origin, len);
+    return 0;
 }
