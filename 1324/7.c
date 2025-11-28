41ee040f7084e797075f75e1dd38ba6516c0d438
lalala_233 <lalala_233@qq.com>
2025-11-28 08:51:14 +0800

    feat: 7

--- 1324/7.c
+++ 1324/7.c
@@ -1,20 +1,35 @@
 // 子串跨度
+#include <ctype.h>
 #include <inttypes.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
 char* read_len(int32_t* len) {
-    char* s = (char*)malloc(*len + 2);
-    fgets(s, *len + 2, stdin);
-    *len = strlen(s);
-    if (s[*len - 1] == '\n') {
-        s[*len - 1] = '\0';
-        *len -= 1;
+    char* s = (char*)malloc(*len + 1);
+    for (int32_t i = 0;; i++) {
+        s[i] = getchar();
+        if (isspace(s[i])) {
+            s[i] = '\0';
+            *len = i;
+            break;
+        }
     }
     return s;
+}
+bool is_substr(const char* origin, const char* substr) {
+    for (int32_t i = 0;; i++) {
+        if (substr[i] == '\0') {
+            return true;
+        } else if (origin[i] == '\0') {
+            return false;
+        } else if (origin[i] != substr[i]) {
+            return false;
+        }
+    }
+}
+int32_t find_left_start(const char* origin, const char* substr, int32_t count) {
+    return -1;
 }
 int32_t main(void) {
     int32_t s_len = 300;
@@ -22,7 +37,33 @@ int32_t main(void) {
     int32_t left_len = 10;
     char* left_s = read_len(&left_len);
     int32_t right_len = 10;
-    char* right_s = read_len(&left_len);
+    char* right_s = read_len(&right_len);
+
+    int32_t left_start = -1;
+    for (int32_t i = 0, search_end = s_len - left_len - right_len; i <= search_end; i++) {
+        if (is_substr(&s[i], left_s)) {
+            left_start = i;
+            break;
+        }
+    }
+    if (left_start == -1) {
+        printf("-1\n");
+        return 0;
+    }
+
+    int32_t right_start = -1;
+    for (int32_t i = s_len - right_len, rsearch_end = left_start + left_len; i >= rsearch_end; i--)
+    {
+        if (is_substr(&s[i], right_s)) {
+            right_start = i;
+            break;
+        }
+    }
+    if (right_start == -1) {
+        printf("-1\n");
+        return 0;
+    }
+    printf("%" PRIi32 "\n", right_start - left_start - left_len);
 
     free(s);
     free(left_s);
