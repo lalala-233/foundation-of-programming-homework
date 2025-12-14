870b84d5b969c3be613e5a9cdd2e20c4bc57c4b3
lalala_233 <lalala_233@qq.com>
2025-12-14 19:30:43 +0800

    fix: use int64_t

--- 1348/2.c
+++ 1348/2.c
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-int32_t merge_count(int32_t* array, int32_t len) {
+int64_t merge_count(int32_t* array, int32_t len) {
     if (len == 1) {
         return 0;
     }
@@ -15,7 +15,7 @@ int32_t merge_count(int32_t* array, int32_t len) {
     int32_t right_len = len - middle;
 
     int32_t* sorted_array = malloc(sizeof(int32_t) * len);
-    int32_t count = 0;
+    int64_t count = 0;
     for (int32_t i = 0, j = 0; i != left_len || j != right_len;) {
         if (i < left_len && j < right_len) {
             if (left[i] <= right[j]) {
@@ -40,9 +40,10 @@ int32_t merge_count(int32_t* array, int32_t len) {
     free(sorted_array);
     return count;
 }
-void merge_sort_count(int32_t* array, int32_t len, int32_t* count) {
+int64_t merge_sort_count(int32_t* array, int32_t len) {
+    int64_t count = 0;
     if (len <= 1) {
-        return;
+        return count;
     }
     int32_t middle = len / 2;
     int32_t* left = array;
@@ -50,10 +51,11 @@ void merge_sort_count(int32_t* array, int32_t len, int32_t* count) {
     int32_t* right = &array[middle];
     int32_t right_len = len - middle;
 
-    merge_sort_count(left, left_len, count);
-    merge_sort_count(right, right_len, count);
+    count += merge_sort_count(left, left_len);
+    count += merge_sort_count(right, right_len);
 
-    *count += merge_count(array, len);
+    count += merge_count(array, len);
+    return count;
 }
 int32_t main(void) {
     int32_t count;
@@ -63,7 +65,7 @@ int32_t main(void) {
         scanf("%" PRIi32, &array[i]);
     }
 
-    int32_t swap_count = 0;
+    int64_t swap_count = merge_sort_count(array, count);
     // for (int32_t i = 0; i < count; i++) {
     //     for (int32_t j = i + 1; j < count; j++) {
     //         if (array[j] < array[i]) {
@@ -73,8 +75,8 @@ int32_t main(void) {
     // }
     // printf("%" PRIi32 "\n", swap_count);
     // swap_count = 0;
-    merge_sort_count(array, count, &swap_count);
-    printf("%" PRIi32 "\n", swap_count);
+
+    printf("%" PRIi64 "\n", swap_count);
     free(array);
     return 0;
 }
